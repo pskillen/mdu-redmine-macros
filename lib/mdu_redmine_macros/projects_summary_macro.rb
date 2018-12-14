09ac54997c321354f966d05cb77f355ff70f81c6
Patrick Skillen <patrick.skillen@nhs.net>
2018-12-14 11:06:17 +0000

    Fix nil vs null bug in projects summary macro

--- lib/mdu_redmine_macros/projects_summary_macro.rb
+++ lib/mdu_redmine_macros/projects_summary_macro.rb
@@ -20,24 +20,24 @@ module MDU
       def do_format(project, custom_fields, _fmt = 'tr')
         roles = project.users_by_role
 
-        pm_role, pm_users = roles.select { |r| r.name == 'Project Lead' }.first
-        tl_role, tl_users = roles.select { |r| r.name == 'Tech Lead' }.first
-        team_role, team_users = roles.select { |r| r.name == 'Project Team' }.first
+        _pm_role, pm_users = roles.select { |r| r.name == 'Project Lead' }.first
+        _tl_role, tl_users = roles.select { |r| r.name == 'Tech Lead' }.first
+        _team_role, team_users = roles.select { |r| r.name == 'Project Team' }.first
         status_wiki = Wiki.find_page('Project Status', project => project)
 
-        code = custom_fields[:code] ? project.custom_field_value(custom_fields[:code].id) : null
+        code = custom_fields[:code] ? project.custom_field_value(custom_fields[:code].id) : nil
 
-        med_dev = custom_fields[:med_dev] ? project.custom_field_value(custom_fields[:med_dev].id) : null
+        med_dev = custom_fields[:med_dev] ? project.custom_field_value(custom_fields[:med_dev].id) : nil
         med_dev = 'No' if med_dev == '0'
         med_dev = 'Yes' if med_dev == '1'
 
-        mdr_class = custom_fields[:mdr_class] ? project.custom_field_value(custom_fields[:mdr_class].id) : null
+        mdr_class = custom_fields[:mdr_class] ? project.custom_field_value(custom_fields[:mdr_class].id) : nil
 
-        charter_ticket = custom_fields[:charter_ticket] ? project.custom_field_value(custom_fields[:charter_ticket].id) : null
+        charter_ticket = custom_fields[:charter_ticket] ? project.custom_field_value(custom_fields[:charter_ticket].id) : nil
         charter_ticket = Issue.find_by_id(charter_ticket) if charter_ticket
         charter_ticket = charter_ticket ? "<a href=\"/issues/#{charter_ticket.id}\">##{charter_ticket.id}</a>" : '(not found)'
 
-        par_ticket = custom_fields[:par_ticket] ? project.custom_field_value(custom_fields[:par_ticket].id) : null
+        par_ticket = custom_fields[:par_ticket] ? project.custom_field_value(custom_fields[:par_ticket].id) : nil
         par_ticket = Issue.find_by_id(par_ticket) if par_ticket
         par_ticket = par_ticket ? "<a href=\"/issues/#{par_ticket.id}\">##{par_ticket.id}</a>" : '(not found)'
 
@@ -53,15 +53,15 @@ module MDU
 
         # TL/PM
         out << '<td><strong>TL:</strong> '
-        out << tl_users.map { |u| link_to_user(u) }.join(', ') if tl_role
+        out << tl_users.map { |u| link_to_user(u) }.join(', ') if tl_users
         out << "<br>\n"
         out << '<strong>PM:</strong> '
-        out << pm_users.map { |u| link_to_user(u) }.join(', ') if pm_role
+        out << pm_users.map { |u| link_to_user(u) }.join(', ') if pm_users
         out << "</td>\n"
 
         # Team
         out << '<td>'
-        out << team_users.map { |u| link_to_user(u) }.join(', ') if team_role
+        out << team_users.map { |u| link_to_user(u) }.join(', ') if team_users
         out << "</td>\n"
 
         # Status Wiki
@@ -105,7 +105,7 @@ module MDU
               code: CustomField.where(type: 'ProjectCustomField', name: 'Code').first,
               charter_ticket: CustomField.where(type: 'ProjectCustomField', name: 'Charter').first,
               par_ticket: CustomField.where(type: 'ProjectCustomField', name: 'PAR').first,
-              med_dev: CustomField.where(type: 'ProjectCustomField', name: 'Medical Device').first,
+              med_dev: CustomField.where(type: 'ProjectCustomField', name: 'Medical device').first,
               mdr_class: CustomField.where(type: 'ProjectCustomField', name: 'MDR Class').first
           }
           raise '- custom field \'Status\' not found' unless fields[:status]
